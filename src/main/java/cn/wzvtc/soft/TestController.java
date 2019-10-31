1d4fd7e731e98d20518b0bf21801d42bd51784fb
chenhao523 <1184773708@qq.com>
2019-10-31 08:51:35 +0800

    all

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -8,10 +8,10 @@ import java.util.Map;
 @CrossOrigin
 @RestController("/" )
 public class TestController {
-    @RequestMapping(value="data.json",method={RequestMethod.GET})
+    @RequestMapping(value="data.json")
     public Map<String,String> bookById(){
         Map<String,String> resultMap=new HashMap<>();
-        resultMap.put("myname","刘笑锋");
+        resultMap.put("myname","刘笑锋996");
         resultMap.put("mynumber","2012020045");
         return resultMap;
     }
